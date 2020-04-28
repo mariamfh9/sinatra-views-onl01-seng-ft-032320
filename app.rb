4e88671339df48ebc59a61a196447c2571772aaf
Mariam Haider <mfh.haider@gmail.com>
2020-04-28 21:55:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
-	end
+      erb :index
+    end
+    
+  get "/info" do
+      erb :info
+    end
+ 
+  get "/info" do
+      "Testing the info page"
+    end
+    
 end
